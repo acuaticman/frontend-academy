ffc249c8c2c57aacca3440628635a8f96e498d82
acuaticman <93457586+acuaticman@users.noreply.github.com>
2023-02-22 23:44:57 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,84 +1,91 @@
-/* 
-var clic = 1;
 
-function divLogin() {
-  if (clic == 1) {
-    document.getElementById("caja").style.visibility = "hidden";
 
-    clic = clic + 1;
-  } else {
-    document.getElementById("caja").style.visibility = "visible";
+// Métodos JS para mostrar ocultar div
 
-    clic = 1;
+function showA () {
+
+    document.getElementById("a").style.display="block"
+    document.getElementById("b").style.display="none"
+    document.getElementById("c").style.display="none"
+    document.getElementById("d").style.display="none"
+    document.getElementById("e").style.display="none"
+    document.getElementById("f").style.display="none"
+    document.getElementById("i").style.display="none"
   }
-}
- */
-var clic = 1;
-
-function mostrar1() {
-  if (clic == 1) {
-    document.getElementById("presentacion").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("presentacion").style.visibility = "hidden";
-
-    clic = 1;
+  
+  function showB () {
+  
+    document.getElementById("a").style.display="none"
+    document.getElementById("b").style.display="block"
+    document.getElementById("c").style.display="none"
+    document.getElementById("d").style.display="none"
+    document.getElementById("e").style.display="none"
+    document.getElementById("f").style.display="none"
+    document.getElementById("i").style.display="none"
   }
-}
-
-var clic = 1;
-
-function mostrar2() {
-  if (clic == 1) {
-    document.getElementById("educacion").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("educacion").style.visibility = "hidden";
-
-    clic = 1;
+  
+  function showC () {
+  
+    document.getElementById("a").style.display="none"
+    document.getElementById("b").style.display="none"
+    document.getElementById("c").style.display="block"
+    document.getElementById("d").style.display="none"
+    document.getElementById("e").style.display="none"
+    document.getElementById("f").style.display="none"
+    document.getElementById("i").style.display="none"
   }
-}
 
-function mostrar3() {
-  if (clic == 1) {
-    document.getElementById("referencias").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("referencias").style.visibility = "hidden";
+  function showD () {
 
-    clic = 1;
+  document.getElementById("a").style.display="none"
+  document.getElementById("b").style.display="none"
+  document.getElementById("c").style.display="none"
+  document.getElementById("d").style.display="block"
+  document.getElementById("e").style.display="none"
+  document.getElementById("f").style.display="none" 
+  document.getElementById("i").style.display="none"
+    }
+
+  function showE () {
+
+  document.getElementById("a").style.display="none"
+  document.getElementById("b").style.display="none"
+  document.getElementById("c").style.display="none"
+  document.getElementById("d").style.display="none"
+  document.getElementById("e").style.display="block"
+  document.getElementById("f").style.display="none"
+  document.getElementById("i").style.display="none"
+    }
+
+  function showF () {
+
+  document.getElementById("a").style.display="none"
+  document.getElementById("b").style.display="none"
+  document.getElementById("c").style.display="none"
+  document.getElementById("d").style.display="none"
+  document.getElementById("e").style.display="none"
+  document.getElementById("f").style.display="block"
+  document.getElementById("i").style.display="none"
+    }
+
+  function hideA () {    
+    document.getElementById("a").style.display="none"
+    document.getElementById("b").style.display="none"
+    document.getElementById("c").style.display="none"
+    document.getElementById("d").style.display="none"
+    document.getElementById("e").style.display="none"
+    document.getElementById("f").style.display="none"
+    document.getElementById("i").style.display="block"
   }
-}
 
-function mostrar4() {
-  if (clic == 1) {
-    document.getElementById("experiencia").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("experiencia").style.visibility = "hidden";
-
-    clic = 1;
+  function showmenu () {
+    document.getElementById("menu-lateral").style.visibility="visible"
+    document.getElementById("boton-open").style.display="none"
+    document.getElementById("boton-close").style.display="block"
   }
-}
 
-function mostrar5() {
-  if (clic == 1) {
-    document.getElementById("habilidad").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("habilidad").style.visibility = "hidden";
-
-    clic = 1;
+  function hidemenu () {
+    document.getElementById("menu-lateral").style.visibility="hidden"
+    document.getElementById("boton-open").style.display="block"
+    document.getElementById("boton-close").style.display="none"
   }
-}
-
-function mostrar6() {
-  if (clic == 1) {
-    document.getElementById("contacto").style.visibility = "visible";
-    clic = clic + 1;
-  } else {
-    document.getElementById("contacto").style.visibility = "hidden";
-
-    clic = 1;
-  }
-}
